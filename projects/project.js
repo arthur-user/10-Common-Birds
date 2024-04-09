5e094e30175ff35e90ef3ff6fb00c7114b5341f3
robotx48 <robotx48@gmail.com>
2024-04-08 23:45:10 -0700

    commit step

--- projects/project.js
+++ projects/project.js
@@ -1,70 +1,36 @@
-const getUserChoice = (userInput) => {
-    userInput = userInput.toLowerCase();
-    if (
-      userInput === "rock" ||
-      userInput === "paper" ||
-      userInput === "scissors" ||
-      userInput === "code"
-    ) {
-      return userInput;
-    } else {
-      console.log("Error!");
-    }
-  };
-  //console.log(getUserChoice("paper"));
-  const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * 3);
-    switch (randomNumber) {
-      case 0:
-        return "rock";
-  
-      case 1:
-        return "paper";
-  
-      case 2:
-        return "scissors";
-    }
-  };
-  //call multiple times
-  /*console.log(getComputerChoice());
-  console.log(getComputerChoice());
-  console.log(getComputerChoice());*/
-  function determineWinner(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-      return "Tie";
-    }
-    if (userChoice === "rock")
-      if (computerChoice === "paper") {
-        return "Computer wins";
-      } else {
-        return "User wins";
-      }
-    if (userChoice === "paper")
-      if (computerChoice === "scissors") {
-        return "Computer wins";
-      } else {
-        return "User wins";
-      }
-  
-    if (userChoice === "scissors")
-      if (computerChoice === "rock") {
-        return "Computer wins";
-      } else {
-        return "User wins";
-      }
-  
-    if (userChoice === "code") {
-      return "Victory";
-    }
-  }
-  
-  const playGame = () => {
-    const userChoice = getUserChoice("rock");
-    console.log(userChoice);
-    const computerChoice = getComputerChoice();
-    console.log(computerChoice);
-  
-    console.log(determineWinner(userChoice, computerChoice));
-  };
-  playGame();
-  
+const randomNumber = (num) => {return Math.floor(Math.random()*num)}
+
+
+
+const birds = {name:['American Robin','Northern Mockingbird','American Goldfinch','White-breasted Nuthatch','Blue Jay',],
+ coolfact1:['Hummingbirds can fly backward and hover in place',
+'Crows hold funerals','Some birds cover themselves in ants',
+'Owls swallow some of their food whole','Pigeons can recognize humans'],
+coolfact2:['Some birds can sleep while flying','The smallest bird egg belongs to the hummingbird',
+'The egg of the ostrich (genus Struthio) is the largest of any living bird','The kiwi is the only wingless bird','Chickens can communicate using over 200 different noises',
+'Owls can turn their heads 270 degrees','Bird eyes take up 50% of their heads'],
+
+}
+
+let randBird = []
+
+//iterating
+for(let i in birds){
+    let randID = randomNumber(birds[i].length)
+
+switch(i){
+    case 'name':
+        randBird.push(`Your random bird is:"${birds[i][randID]}"`)
+        break
+    case 'coolfact1':
+        randBird.push(`Cool facts about birds: "${birds[i][randID]}"`)
+        break
+    case 'coolfact2':
+        randBird.push(`Cool facts about birds: "${birds[i][randID]}"`)
+        break
+    default:
+        randBird.push('Please try again')
+}
+
+}
+
