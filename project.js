ca62564bc75b4230a9c47f05a1eb7e8e6b33b8bf
arthur-user <115193136+arthur-user@users.noreply.github.com>
2024-04-09 23:04:46 -0700

    Update project.js

--- project.js
+++ project.js
@@ -3,10 +3,10 @@ const randomNumber = (num) => {return Math.floor(Math.random()*num)}
 
 
 const birds = {name:['American Robin','Northern Mockingbird','American Goldfinch','White-breasted Nuthatch','Blue Jay',],
- coolfact1:['Hummingbirds can fly backward and hover in place',
+ interestingFact1:['Hummingbirds can fly backward and hover in place',
 'Crows hold funerals','Some birds cover themselves in ants',
 'Owls swallow some of their food whole','Pigeons can recognize humans'],
-coolfact2:['Some birds can sleep while flying','The smallest bird egg belongs to the hummingbird',
+interestingFact2:['Some birds can sleep while flying','The smallest bird egg belongs to the hummingbird',
 'The egg of the ostrich (genus Struthio) is the largest of any living bird','The kiwi is the only wingless bird','Chickens can communicate using over 200 different noises',
 'Owls can turn their heads 270 degrees','Bird eyes take up 50% of their heads'],
 
